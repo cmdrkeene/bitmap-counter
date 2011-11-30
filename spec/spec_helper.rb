1e7fbf1873b92b295e80a241648a83e8c41d56e4
Brandon Keene <bkeene@gmail.com>
2011-11-30 02:37:44 -0500

    Remove rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,3 @@ require "bundler/setup"
 Bundler.require :default, :development
 
 require_relative "../lib/bitmap_counter"
-
-RSpec.configure do |config|
-  config.before(:each) do
-    # ???
-  end
-end
